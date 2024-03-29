26d097f7b017643a7eebb2b3eb9268f5ff19b4d8
Periklis <perik76@gmail.com>
2024-03-29 03:14:00 +0200

    Almost done-extension done- but no new dogbutton on top

--- src/index.js
+++ src/index.js
@@ -83,7 +83,6 @@ const addNaughtyOrGoodAnswer = (dog) => {
 	return dogJudgementAnswer
 }
 
-//Judge button 
 function naughtyOrGoodBtn(dogs) {
 	let naughtyBtnTxt
 	if (dogs.isGoodDog) {
@@ -102,8 +101,6 @@ function addJudgeButton(dog) {
 	return judgeButton
 }
 
-
-// Assemble the card
 function createDogCard(dog) {
 	const dogName = addDogName(dog)
 	dogCard.append(dogName)
@@ -130,12 +127,129 @@ function createDogCard(dog) {
 	})
 }
 
-// Handle top menu clicks and construct card to display
+// Create add-dog form
 
+function makeAddDogFormSection() {
+	const formSection = document.createElement("section")
+	formSection.classList.add("main__dog-section")
+	return formSection
+}
+
+function makeFormTitle() {
+	const formTitle = document.createElement("h2")
+	formTitle.innerText = "Add a new Dog"
+	return formTitle
+}
+
+function makeAddDogForm() {
+	const newDogForm = document.createElement("form")
+	newDogForm.classList.add("form")
+	return newDogForm
+}
+
+function makeNameInputLabel() {
+	const nameInputLabel = document.createElement("label")
+	nameInputLabel.setAttribute("for", "name")
+	nameInputLabel.innerText = "Dog's name"
+	return nameInputLabel
+}
+
+function makeNameInput() {
+	const nameInput = document.createElement("input")
+	nameInput.setAttribute("type", "text")
+	nameInput.setAttribute("id", "name")
+	nameInput.setAttribute("name", "name")
+	return nameInput
+}
+
+function makeImageInputLabel() {
+	const imageInputLabel = document.createElement("label")
+	imageInputLabel.setAttribute("for", "Image")
+	imageInputLabel.innerText = "Dog's picture"
+	return imageInputLabel
+}
+
+function makeImageInput() {
+	const imageInput = document.createElement("img")
+	imageInput.setAttribute("type", "url")
+	imageInput.setAttribute("id", "image")
+	imageInput.setAttribute("name", "image")
+	return imageInput
+}
+
+function makeBioInputLabel() {
+	const bioInputLabel = document.createElement("label")
+	bioInputLabel.setAttribute("for", "bio")
+	bioInputLabel.innerText = "Dog's bio"
+	return bioInputLabel
+}
+
+function makeBioInput() {
+	const bioInput = document.createElement("textarea")
+	bioInput.setAttribute("id", "bio")
+	bioInput.setAttribute("name", "bio")
+	bioInput.setAttribute("rows", 5)
+	return bioInput
+}
+
+function makeFormSubmitBtn() {
+	const submitForm = document.createElement("input")
+	submitForm.setAttribute("id", "submit")
+	submitForm.setAttribute("type", "submit")
+	submitForm.setAttribute("name", "submit")
+	submitForm.setAttribute("value", "Let's add a dog!")
+	submitForm.classList.add("form__button")
+	return submitForm
+}
+
+function makeAddDogInterface() {
+	const section = makeAddDogFormSection()
+	main.append(section)
+	const form = makeAddDogForm()
+	section.append(form)
+	const title = makeFormTitle()
+	form.append(title)
+	const nameLabel = makeNameInputLabel()
+	form.append(nameLabel)
+	const addName = makeNameInput()
+	form.append(addName)
+	const imageLabel = makeImageInputLabel()
+	form.append(imageLabel)
+	const addImage = makeImageInput()
+	form.append(addImage)
+	const bioLabel = makeBioInputLabel()
+	form.append(bioLabel)
+	const addBio = makeBioInput()
+	form.append(addBio)
+	const submitButton = makeFormSubmitBtn()
+	form.append(submitButton)
+}
+
+// function addNewDoggie() {
+// 	const newDoggieObj = {
+// 		id: null,
+// 		name: null,
+// 		bio: null,
+// 		isGoodDog: true,
+// 		image: null,
+// 	}
+// 	const doggieName = document.getElementById("name").value
+// 	const doggieImage = document.getElementById("image").value
+// 	const doggieBio = document.getElementById("bio").value
+// 	newDoggieObj.id = dogs.length + 1
+// 	newDoggieObj.name = doggieName
+// 	newDoggieObj.image = doggieImage
+// 	newDoggieObj.bio = doggieBio
+// 	dogs.unshift(newDoggieObj)
+
+// }
+
+// Handle top menu clicks and display cards/form
 function handleDogButtonClick(dogs) {
 	topMenu.addEventListener("click", (event) => {
+		event.preventDefault()
 		const clickedButton = event.target
-
+		dogCard.innerHTML = ""
 		if (clickedButton.classList.contains("dog-button")) {
 			// Find the index of the clicked dog in the array
 			const index = Array.from(topMenu.querySelectorAll(".dog-button")).indexOf(
@@ -143,31 +257,17 @@ function handleDogButtonClick(dogs) {
 			)
 			// Clear existing content in dogCard
 			dogCard.innerHTML = ""
-
-			// Call card construction
 			createDogCard(dogs[index])
+		} else {
+			clearMain()
+			makeAddDogInterface()
 		}
 	})
 }
 
 handleDogButtonClick(dogs)
 
-// Create add dog form
-
-function makeAddDogForm() {
-	const formSection = document.createElement("section")
-	formSection.classList.add("main__dog-section")
+function clearMain() {
+	const oldCard = document.querySelector(".main__dog-section")
+	oldCard.remove()
 }
-
-function makeFormTitle() {
-	const formTitle = document.createElement("h2")
-	formTitle.innerText = "Add a new Dog"
-}
-
-function makeNameInput() {
-	const nameInput = document.createElement
-}
-
-// function makeAddDogNameField() {
-// 	const addNameField
-// }
